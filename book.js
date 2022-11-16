87a0b3519e9c06f1f1250cabec8f8d84be7e5714
PakeezaFatma <you@example.com>
2022-11-16 23:32:18 +0530

    book_appo_edit

--- book.js
+++ book.js
@@ -1,20 +1,10 @@
 function saveToLocalStorage(event)
 {
-    //  const name = document.getElementById('name').value;
-    // console.log(name);
-    // const email = document.getElementById('email').value;
-    // console.log(email);
-    // const phone = document.getElementById('phone').value;
-    // console.log(phone);
-    // const date = document.getElementById('date').value;
-    // console.log(date);
-
-
-    //Now try storing the user details as an object in the local storage. 
+    
     event.preventDefault();//its stoping the unnecessary reloading page
 
     const name = event.target.username.value;
-    const email = event.target.emailid.value;
+    const email = event.target.emailId.value;
     const phone = event.target.phoneNumber.value;
     const  date = event.target.date.value;
     
@@ -30,14 +20,73 @@ function saveToLocalStorage(event)
     
     showNewUserOnScreen( obj )
     
-
+    
     
 
 }
-function showNewUserOnScreen(user)
-{
+window.addEventListener("DOMContentLoaded",() => {
+    const localStorageObj = localStorage;
+    const localStorageKeys = Object.keys(localStorageObj);
+
+    for (let i =0; i<localStorageKeys.length; i++){
+        const key = localStorageKeys[i];
+        const userDetailsString = localStorageObj[key];
+        const userDetailsObj = JSON.parse(userDetailsString);
+        showNewUserOnScreen(userDetailsObj)
+    }
+   });
+
+
+
+
+function showNewUserOnScreen(user){
+
+   
+    if(localStorage.getItem(user.email) !== null)
+    {
+        removeUserFromScreen(user.email)
+    }
     const parentNode = document.getElementById('listOfUsers');
-    const childHTML = `<li> ${user.name}-${user.email} </li>`
+    const childHTML = `<li id=${user.email}> '${user.name}-${user.email}'
+    <button onclick=deleteUser('${user.email}')> Delete User </button>
+    <button onclick=editUserDetails('${user.email}','${user.name}','${user.phone}')>Edit User </button>
+    
+    </li>` 
+
     parentNode.innerHTML = parentNode.innerHTML + childHTML;
+
 }
 
+    
+
+function deleteUser(emailId){
+    console.log(emailId)
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+}
+
+
+function removeUserFromScreen(emailId)
+{
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(emailId);
+    if(childNodeToBeDeleted){
+        parentNode.removeChild(childNodeToBeDeleted)
+    
+    }
+    
+}
+            //Edit User
+
+function editUserDetails(emailId,name,phoneNumber){
+
+    document.getElementById('emailId').value = emailId;
+
+    document.getElementById('username').value = name;
+    document.getElementById('phoneNumber').value = phoneNumber;
+    console.log(document.getElementById('username').value = name);
+
+    deleteUser(emailId)
+    //deleteUser(userId)
+
+}
